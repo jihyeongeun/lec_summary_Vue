34f11a86183402a360ec3d19efcbcb656a1a36c2
jihyeongeun <hyeongeun0924@gmail.com>
2024-03-25 16:01:13 +0900

    03.25 lecture
    dynamicslot finish

---
+++ 06_slot/src/components/01_component/MyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+    <h1>MyComponent</h1>
+<script>
+import { onMounted } from 'vue';
+
+export default {
+        setup(){
+            onMounted(async() => {
+             console.log("Hello!");   
+            })
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
