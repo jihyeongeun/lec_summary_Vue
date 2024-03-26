e630b3ba5e486d007365dc18f84bf9c0d39d8b89
jihyeongeun <hyeongeun0924@gmail.com>
2024-03-26 09:52:41 +0900

    03.26 lecture

---
+++ 07_props/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import PropsParent from './components/01_propsBasic/PropsParents.vue'
+</script>
+
+<template>
+  <header>
+    <PropsParent/>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
